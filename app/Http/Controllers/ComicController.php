2331d4efda831233a26f1b0d07596ff84a20f67e
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-18 17:44:04 +0200

    add function comic create

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -25,7 +25,7 @@ class ComicController extends Controller
      */
     public function create()
     {
-        //
+        return view('comic.create');
     }
 
     /**
@@ -36,7 +36,12 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data= $request->all();
+        $newComic = new Comic();
+        $newComic->fill($data);
+        $newComic->save();
+
+        return redirect()->route('comic.index');
     }
 
     /**
