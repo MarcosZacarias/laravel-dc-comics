fe27a2c4a9d11ed10d1da5c88fa12a5930d913a1
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-18 16:28:31 +0200

    edit: cards comics

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -7,21 +7,17 @@
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
                 @forelse($comics as $comic)
                 <div class="col">              
-                    <div class="card h-100">
-                        <img src="{{$comic->thumb}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h3 class="card-title"><strong>{{$comic->title}}</strong></h3>
-                        <p class="card-text">
-                           <strong>Description: </strong> {{$comic->description}}
-                        </p>
-                        <ul>
-                            <li><strong>Price: </strong>{{$comic->price}}</li>
-                            <li><strong>Series: </strong>{{$comic->series}}</li>
-                            <li><strong>Sale Date: </strong>{{$comic->sale_date}}</li>
-                            <li><strong>Type: </strong>{{$comic->type}}</li>
-                        </ul>
+                  <div class="card h-100">
+                    
+                    <img src="{{$comic->thumb}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h3 class="card-title"><strong>{{$comic->title}}</strong></h3>
+                      <a href="{{ route('comic.show', $comic->id) }}">
+                        <button class="btn btn-primary">More info</button>
+                      </a>
                       </div>
-                    </div>              
+                    </div> 
+
                 </div>
                 @empty
                 <h2>Non ci sono risultati</h2>
