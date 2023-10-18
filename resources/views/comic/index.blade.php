2762f5e67d2bea20b5ec47f266ec04cdeaa915a8
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-18 16:48:36 +0200

    fix: change layout comics

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -1,29 +1,47 @@
 @extends('layouts.app')
 
-@section('main-content')
+@section('main-content')  
   <section class="container mt-5">
-    <section class="container mt-5">
-        <div class="container">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
-                @forelse($comics as $comic)
-                <div class="col">              
-                  <div class="card h-100">
-                    
-                    <img src="{{$comic->thumb}}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h3 class="card-title"><strong>{{$comic->title}}</strong></h3>
-                      <a href="{{ route('comic.show', $comic->id) }}">
-                        <button class="btn btn-primary">More info</button>
-                      </a>
-                      </div>
-                    </div> 
+    <a href="{{ route('comic.create') }}" class="">
+      <button class="btn btn-primary mb-3">Add Comic</button>
+    </a>
 
-                </div>
-                @empty
-                <h2>Non ci sono risultati</h2>
-                @endforelse
-            </div>
-        </div>
-      </section>
-  </section>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Title</th>
+          {{-- <th scope="col">Description</th> --}}
+          <th scope="col">Thumb</th>
+          <th scope="col">Price</th>
+          <th scope="col">Series</th>
+          <th scope="col">Sale date</th>
+          <th scope="col">Type</th>          
+          <th scope="col">Info</th>          
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($comics as $comic)
+            
+        <tr>
+          <th scope="row">{{$comic->id}}</th>
+          <th scope="col">{{$comic->title}}</th>
+          {{-- <th scope="col">{{$comic->description}}</th> --}}
+          <th scope="col">{{$comic->thumb}}</th>
+          <th scope="col">{{$comic->price}}</th>
+          <th scope="col">{{$comic->series}}</th>
+          <th scope="col">{{$comic->sale_date}}</th>
+          <th scope="col">{{$comic->type}}</th>  
+          <th scope="col">
+            <a href="{{ route('comic.show', $comic->id) }}">
+              <button class="btn btn-info">More info</button>
+            </a>
+          </th>  
+        </tr>
+        @endforeach
+
+        
+      </tbody>
+    </table>
+  </section> 
 @endsection
