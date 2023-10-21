5294febccd8070751338892883ac9e319b8f6b72
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-21 13:15:47 +0200

    add layout with function layout

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -8,6 +8,18 @@
         </a>
 
         <h1>Create Comic</h1>
+
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <h4>Correct the errors:</h4>
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
         <form action="{{route('comic.store')}}" method="POST">
             @csrf
             <div class="row">
@@ -18,28 +30,76 @@
                     <div class="row g-4">
                         <div class="col-4">
                             <label for="title" class="form-label">Title</label>
-                            <input type="text" class="form-control" id="title" name="title">
+                            <input 
+                                type="text" 
+                                class="form-control @error('title') is-invalid @enderror" 
+                                id="title" 
+                                name="title"
+                                value="{{old('title')}}">
+                                @error('title')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                         </div>
                         <div class="col-8">
                             <label for="thumb" class="form-label">Link img</label>
-                            <input type="url" class="form-control" id="thumb" name="thumb">
+                            <input 
+                                type="url" 
+                                class="form-control @error('thumb') is-invalid @enderror" 
+                                id="thumb" 
+                                name="thumb"
+                                value="{{old('thumb')}}">
+                                @error('thumb')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                         </div>
                         <div class="col-12">
                             <label for="description" class="form-label">Description</label>
-                            <textarea type="text" class="form-control" id="description" name="description">
+                            <textarea 
+                                type="text" 
+                                class="form-control @error('description') is-invalid @enderror" 
+                                id="description" 
+                                name="description">
+                                {{old('description')}}
                             </textarea>
+                            @error('description')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="col-3">
                             <label for="price" class="form-label">Price</label>
-                            <input type="number" class="form-control" id="price" name="price">
+                            <input 
+                                type="number" 
+                                class="form-control @error('price') is-invalid @enderror" 
+                                id="price" 
+                                name="price"
+                                value="{{old('price')}}">
+                            @error('price')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="col-3">
                             <label for="series" class="form-label">Series</label>
-                            <input type="text" class="form-control" id="series" name="series">
+                            <input 
+                                type="text" 
+                                class="form-control @error('series') is-invalid @enderror" 
+                                id="series" 
+                                name="series"
+                                value="{{old('series')}}">
+                            @error('series')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="col-3">
                             <label for="sale_date" class="form-label">Sale Date</label>
-                            <input type="date" class="form-control" id="sale_date" name="sale_date">                    
+                            <input 
+                                type="date" 
+                                class="form-control @error('sale_date') is-invalid @enderror" 
+                                id="sale_date" 
+                                name="sale_date"
+                                value="{{old('sale_date')}}"> 
+                            @error('sale_date')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror                   
                         </div>
                         <div class="col-3">
                             <label for="type" class="form-label">Type</label>
